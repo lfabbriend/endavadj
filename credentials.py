ff20432abfdeb1dca201813304a220e3c77103cf
lucia.fabbri <lucia.fabbri@endava.com>
2022-09-21 15:58:35 -0300

    removed file

--- credentials.py
+++
@@ -1,4 +0,0 @@
-client_id = '15c9e02f33f94eac8ca59c572e1c3ce1'
-client_secret = '48612e0078554a919c469bef4ccfad77'
-redirect_uri = "http://localhost:7777/callback"
-scope = "user-read-recently-played"
